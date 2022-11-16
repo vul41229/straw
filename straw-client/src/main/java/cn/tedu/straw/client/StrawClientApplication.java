e747bd7930742c765505124b63019d24039e2c41
vul41229 <111473707+vul41229@users.noreply.github.com>
2022-11-16 09:57:29 +0800

    Add files via upload

---
+++ straw-client/src/main/java/cn/tedu/straw/client/StrawClientApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.straw.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrawClientApplication.class, args);
+    }
+
+}
